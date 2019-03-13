2da57edfbdfe20afa09488d66c8243945bf895bb
Alex <aly_t99@yahoo.com>
2019-03-13 14:37:09 +0000

    adding stuff

---
+++ frontend/script/phpintegrate.js
@@ -0,0 +1,43 @@
+const xHttpReq = new XMLHttpRequest();
+
+let dynamicContent = ["header", "footer"];
+
+window.onload = function()
+{
+    const header = document.getElementById("header");
+    const content = document.getElementById("content");
+    const footer = document.getElementById("footer");
+
+    dynamicContent.forEach(function(page)
+    {
+        contentOnServer('get', (page + ".html"), 
+                        null, document.getElementById(page));
+    });
+    setHeight();
+}
+
+function contentOnServer(request, content, parameter, location)
+{
+    xHttpReq.open(request , content, false);
+
+    xHttpReq.onreadystatechange = function()
+    {
+        if(this.readyState == 4)
+        {
+            if(this.status == 200)
+               location.innerHTML = this.responseText;
+
+            if(this.status == 404)
+                console.warn("File not found!");
+        }
+    }
+
+    //xHttpReq.setRequestHeader("a", "aa");
+    xHttpReq.send(parameter);
+}
+
+function setHeight()
+{
+    content.style.height = document.documentElement.clientHeight; 
+    console.log(content.clientHeight);
+}
